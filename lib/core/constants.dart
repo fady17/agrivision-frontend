a60a64fac82f09f35905f76f398441fbfdbe7b61
fady17 <fady17@github.com>
2026-03-02 00:16:27 +0200

    clone

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   // Your live backend URL
-  static const String baseUrl = 'https://agrivision.orjnz.com';
+  static const String baseUrl = 'http://localhost:8080';
   static const String analyzeEndpoint = '/api/v1/analyze';
 }
