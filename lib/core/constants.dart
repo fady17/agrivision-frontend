83a9ba77aa5ab234894f06cc80a7b84d41e60118
fady17 <fady17@github.com>
2026-03-05 21:35:00 +0200

    fix:localhost to 0.0.0.0

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   // Your live backend URL
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'http://0.0.0.0:8000';
   static const String analyzeEndpoint = '/api/v1/analyze';
 }
